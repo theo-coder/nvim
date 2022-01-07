9b9f9669dc6aeffb728c9185b45d0d5f2ed1f468
Théo Posty <theo@idfr.net>
2022-01-07 20:54:40 +0100

    add indentline + impatient plugins

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -52,6 +52,8 @@ return packer.startup(function(use)
 	use("moll/vim-bbye")
     use("nvim-lualine/lualine.nvim")
     use("ahmedkhalf/project.nvim")
+    use("lewis6991/impatient.nvim")
+    use("lukas-reineke/indent-blankline.nvim")
 
 	-- Colorschemes
 	use("lunarvim/colorschemes") -- A bunch of colorschemes you can try out
--- plugin/packer_compiled.lua
+++ plugin/packer_compiled.lua
@@ -134,6 +134,16 @@ _G.packer_plugins = {
     path = "/home/theo/.local/share/nvim/site/pack/packer/start/gitsigns.nvim",
     url = "https://github.com/lewis6991/gitsigns.nvim"
   },
+  ["impatient.nvim"] = {
+    loaded = true,
+    path = "/home/theo/.local/share/nvim/site/pack/packer/start/impatient.nvim",
+    url = "https://github.com/lewis6991/impatient.nvim"
+  },
+  ["indent-blankline.nvim"] = {
+    loaded = true,
+    path = "/home/theo/.local/share/nvim/site/pack/packer/start/indent-blankline.nvim",
+    url = "https://github.com/lukas-reineke/indent-blankline.nvim"
+  },
   ["lualine.nvim"] = {
     loaded = true,
     path = "/home/theo/.local/share/nvim/site/pack/packer/start/lualine.nvim",
